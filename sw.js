3dd3efeacfbc306962c69d85ee24249d90518527
manperez1986-ux <manperez1986@gmail.com>
2026-03-01 21:14:42 -0400

    sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'coach-manu-v3';
+const assets = ['./', './index.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+  )));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+});
